5752892e24348f6466ebc69ba99215e2bb463bd9
alrossi <arossi@otfrid.fnal.gov>
2015-01-21 13:53:39 -0600

    pool manager modifications

--- modules/dcache/src/main/java/diskCacheV111/poolManager/PGroup.java
+++ modules/dcache/src/main/java/diskCacheV111/poolManager/PGroup.java
@@ -3,14 +3,44 @@ package diskCacheV111.poolManager;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.google.common.base.Preconditions;
 import diskCacheV111.poolManager.PoolSelectionUnit.SelectionPoolGroup;
 
 class PGroup extends PoolCore implements SelectionPoolGroup {
     private static final long serialVersionUID = 3883973457610397314L;
     final Map<String, Pool> _poolList = new ConcurrentHashMap<>();
 
+    private int minReplicas = 1;
+    private int maxReplicas = 1;
+    private boolean sameHostEnabled = true;
+
     PGroup(String name) {
         super(name);
+    }
+
+    @Override
+    public boolean areSameHostReplicasEnabled() {
+        return sameHostEnabled;
+    }
+
+    @Override
+    public int getMaxReplicas() {
+        return maxReplicas;
+    }
+
+    @Override
+    public int getMinReplicas() {
+        return minReplicas;
+    }
+
+    public void setMaxReplicas(int maxReplicas) {
+        Preconditions.checkArgument(maxReplicas > 0);
+        this.maxReplicas = maxReplicas;
+    }
+
+    public void setMinReplicas(int minReplicas) {
+        Preconditions.checkArgument(minReplicas > 0);
+        this.minReplicas = minReplicas;
     }
 
     private String[] getPools()
@@ -18,9 +48,25 @@ class PGroup extends PoolCore implements SelectionPoolGroup {
         return _poolList.keySet().toArray(new String[_poolList.size()]);
     }
 
-    @Override
-    public String toString() {
-        return getName() + "  (links=" + _linkList.size() + ";pools=" + _poolList.size() + ")";
+    public void setSameHostReplicasEnabled(boolean sameHostEnabled) {
+        this.sameHostEnabled = sameHostEnabled;
     }
 
+    public void validate() throws IllegalStateException {
+        if (minReplicas > maxReplicas) {
+            throw new IllegalStateException(this + ", minReplicas "
+                            + "exceeds maxReplicas " + minReplicas
+                            + " > " + maxReplicas);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                        + "  (links=" + _linkList.size()
+                        + ";pools=" + _poolList.size()
+                        + "; minReplicas=" + minReplicas
+                        + "; maxReplicas=" + maxReplicas
+                        + "; sameHostEnabled=" + sameHostEnabled + ")";
+    }
 }
